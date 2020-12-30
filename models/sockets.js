cf2e481a9a6751d66f1fbc7d23387d68f9071be6
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-12-30 09:12:12 -0300

    Marcadores model

--- models/sockets.js
+++ models/sockets.js
@@ -1,31 +1,23 @@
-
+const Marcadores = require('./marcadores');
 
 class Sockets {
+  constructor(io) {
+    this.io = io;
+    this.marcadores = new Marcadores();
+    this.socketEvents();
+  }
 
-    constructor( io ) {
+  socketEvents() {
+    // On connection
+    this.io.on('connection', (socket) => {
+      // Escuchar evento: mensaje-to-server
+      socket.on('mensaje-to-server', (data) => {
+        console.log(data);
 
-        this.io = io;
-
-        this.socketEvents();
-    }
-
-    socketEvents() {
-        // On connection
-        this.io.on('connection', ( socket ) => {
-
-            // Escuchar evento: mensaje-to-server
-            socket.on('mensaje-to-server', ( data ) => {
-                console.log( data );
-                
-                this.io.emit('mensaje-from-server', data );
-            });
-            
-        
-        });
-    }
-
-
+        this.io.emit('mensaje-from-server', data);
+      });
+    });
+  }
 }
-
 
 module.exports = Sockets;
